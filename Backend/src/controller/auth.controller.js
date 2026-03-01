09c2f638fa3e646554fc9eb18e671de6e73f22e2
Prerna Sharma <prernasharma0925@gamil.com>
2026-03-01 13:43:38 +0530

    Implement authentication middleware and user management functions

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const blacklistModel = require("../models/blacklist.model")
+const blacklistModel = require("../models/blacklist.model")
 
 async function registerUser(req, res) {
     const { username, email, password } = req.body;
@@ -99,6 +99,25 @@ async function loginUser(req, res) {
     })
 }
 
+async function getMe(req,res) {
+    const user = await userModel.findById(req.user.id).select("-password")
+
+    res.status(200).json({
+        message: "User fetched successfully",
+        user
+    })
+}
+
+async function logoutUser(req, res){
+
+    const token = req.cookies.token
+
+    res.clearCookie("token")
+
+    await blacklistModel.create({
+        message: "logout successfully",
+    })
+}
 
 
-module.exports = { registerUser, loginUser }
+module.exports = { registerUser, loginUser, getMe, logoutUser }
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const authController = require("../controller/auth.controller");
-// const authMiddleware = require("../middlewares/auth.middleware")
+const authMiddleware = require("../middlewares/auth.middleware")
 const router = Router();
 
 
@@ -10,7 +10,7 @@ router.post('/register', authController.registerUser)
 router.post('/login', authController.loginUser)
 
 
-// router.get("/get-me", authMiddleware.authUser, authController.getMe)
+router.get("/get-me", authMiddleware.authUser, authController.getMe)
 
 // router.get("/logout",authController.logoutUser)
 
